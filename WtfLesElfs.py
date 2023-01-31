1d432252adae10b8e1f36b81e21fddfe14721a1e
Olivier <76748500+aweirdwhale@users.noreply.github.com>
2023-01-31 18:52:28 +0100

    partie 1 + 2

--- WtfLesElfs.py
+++ WtfLesElfs.py
@@ -1,4 +1,4 @@
-InputProjet2=open('Input2-ProjetsTypesConstruits.>
+InputProjet2=open('Input2-ProjetsTypesConstruits.txt','r')
 VariableProjet=InputProjet2.read()
 
 a = VariableProjet.split('\n')
@@ -16,11 +16,14 @@ cailloux = 1
 feuille = 2
 ciseaux = 3
 
+
+                  
+#partie 1
 #admettons que les elfs jouent 1500 manches:
 score = 0
 
 for meh in range(len(a)):
-        #manche avec le cailloux
+        #manches avec le cailloux
         if a[meh] == 'A Z':
                 score = score+ won+cailloux
         elif a[meh] == 'A Y':
@@ -46,4 +49,36 @@ for meh in range(len(a)):
 
 
 #on imprime le score a la fin
-pri
+print(score)
+                  
+                  
+#partie 2
+for meh2 in range(len(a)):
+    #manches avec le cailloux (adversaire)
+        if a[meh] == 'A Z': #gagner contre la pierre, donc utiliser feuille
+                score = score + won + feuille
+        elif a[meh] == 'A Y': #nul contre pierre, donc pierre
+                score = score + nul + cailloux
+        elif a[meh] == 'A X': #perdre contre pierre, donc ciseaux
+                score = score + lost + ciseaux
+
+        # ... etc ...
+        
+        #avec la feuille (adversaire)
+        elif a[meh] == 'B Z':
+                score = score + won + ciseaux
+        elif a[meh] == 'B Y':
+                score = score + nul + feuille
+        elif a[meh] == 'B X':
+                score = score + lost + cailloux
+
+        #avec les ciseaux (adversaire)
+        elif a[meh] == 'C Z':
+                score =score + cailloux + won
+        elif a[meh] == 'C Y':
+                score =score + ciseaux + nul
+        elif a[meh] == 'C X':
+                score =score + feuille + lost
+                
+#on imprime le score à la fin
+print(score)
